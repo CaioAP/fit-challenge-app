80ec7d983aad1512f200a678f49d6f083de9c137
Caio Alfonso <caio.alfonso@cilia.com.br>
2024-02-08 10:38:46 -0300

    Created BaseImage component

---
+++ src/components/BaseImage.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+type SrcType =
+  | string
+  | { src: string; srcset: string; lazySrc: string; aspect: number };
+const props = defineProps({
+  width: String || Number,
+  height: String || Number,
+  aspectRatio: String || Number,
+  cover: Boolean,
+  rounded: String || Number || Boolean,
+  src: String || (Object as () => SrcType)
+});
+</script>
+
+<template>
+  <v-img
+    :width="props.width"
+    :height="props.height"
+    :aspect-ratio="props.aspectRatio"
+    :cover="props.cover"
+    :rounded="props.rounded"
+    :src="props.src"
+  />
+</template>
+
+<style scoped></style>
