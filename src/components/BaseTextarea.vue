fbd9d7990b873c1f30a8bba7748f2b587b95956e
Caio Alfonso <caio.alfonso@cilia.com.br>
2024-02-21 14:13:13 -0300

    Created a global textarea input component

---
+++ src/components/BaseTextarea.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+type VariantType =
+  | 'underlined'
+  | 'outlined'
+  | 'filled'
+  | 'solo'
+  | 'solo-inverted'
+  | 'solo-filled'
+  | 'plain';
+const props = defineProps({
+  label: String,
+  placeholder: String,
+  grow: Boolean,
+  rows: Number,
+  rowHeight: Number,
+  variant: {
+    type: String as () => VariantType,
+    default: 'outlined'
+  }
+});
+const model = defineModel();
+</script>
+
+<template>
+  <v-textarea
+    v-model.trim="model"
+    :label="props.label"
+    :auto-grow="props.grow"
+    :placeholder="props.placeholder"
+    :variant="props.variant"
+    :rows="props.rows"
+    :row-height="props.rowHeight"
+    hide-details="auto"
+  />
+</template>
+
+<style scoped></style>
